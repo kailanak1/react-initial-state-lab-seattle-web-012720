be39912a39d348e49e2df75e28c49cf140fa4488
kailana <kailanakahawaii@gmail.com>
2020-03-30 17:01:23 -0700

    Done.

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,29 @@
 // your Bomb code here!
+
+import React, { Component } from 'react';
+
+export default class Bomb extends React.Component {
+    constructor(props) {
+        super()
+        this.state = {
+            secondsLeft: props.initialCount
+        }
+    }
+    countDown = () => {
+        if (this.props.initialCount > 0){
+          return  `${this.props.initialCount} ` + `seconds left before I go boom!`
+        }else{
+          return `Boom!`
+        }
+      }
+
+    render (){
+        console.log(this.props)
+        return(
+            <div>
+               {this.countDown()}
+            </div>
+        )
+
+    }
+}
